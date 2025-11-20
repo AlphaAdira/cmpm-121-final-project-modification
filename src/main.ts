524ec81cd850e4137b6e6bc08d116bffe18304fa
Adira Rana <58012161+AlphaAdira@users.noreply.github.com>
2025-11-20 21:04:03 +0000

    adjust starting position of target ground to 5, -3, 0

--- src/main.ts
+++ src/main.ts
@@ -65,7 +65,7 @@ const enableCameraControls = false;
   // Add a target ground
   const targetGround = physics.addBox(
     new THREE.Vector3(5, 1, 20), // Target ground size
-    new THREE.Vector3(0, -3, 0), // Starting position
+    new THREE.Vector3(5, -3, 0), // Starting position
     0, // Mass (0 = static object)
     0xff00ff, // Magenta
   );
