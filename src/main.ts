d2053a4001b71ad6ed53e147cb2adb73525741b8
nbilledo <nbilledo@ucsc.edu>
2025-11-29 03:42:06 +0000

    Adjusting the prepush hook

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,6 @@ const enableCameraControls = false;
 
   // Create scene
   const scene = new THREE.Scene();
-  const unusedInt: number = 5;
 
   // ---------- Camera Controls ----------
   // Create camera
